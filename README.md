8e0236718209adf5f631e750488760ad12a024cd
Chris Masterton <crmasterton@gmail.com>
2021-03-11 10:09:15 -0800

    Weighted pool scriptable object

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ Apply tags to GameObjects at runtime or through the Unity editor. You can apply 
 
 Search for all objects with a specific tag:
 	List<GameObject> Taggy.Find(string objectsWithThisTag);
+
+# Weighted Pool
+Scriptable object useful for reward pools, or any time you want randomness but with pre-defined weights. I suppose you could also change the weights at runtime too.
+
+Create the asset then add whatever objects you want to randomly pick between. You can use any range of values and it does not need to add up to 100 (or 1.0). The weights are relative to each other. For example:
+
+Item: Crappy sword
+Weight: 500
+
+Item: Sparkly sword
+Weight: 100
+
+Item: Amazing super and totally the best sword
+Weight: 5
+
+WeightedPoolAsset.Random() will return you the weighted and random entry - which will mostly likely be a Crappy Sword because we have weighted is so highly compared to the others.
+
---
+++ WeightedPool/WeightedPoolAsset.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Weighted Pool")]
+public class WeightedPoolAsset : ScriptableObject
+{
+    [Serializable]
+    public class Entry
+    {
+        public GameObject contextGameObject;	// but this can be anything really, an enum, int, string, prefab.
+        public float weight;
+    };
+
+    public Entry[] entries;
+
+    public Entry Random()
+    {
+        Assert.IsNotNull(entries, "Weighted Pool " + this.name + " has zero entries");
+        
+        var totalWeight = 0.0f; // this stores sum of weights of all elements before current
+        var selected = entries[0];
+        foreach (var entry in entries)
+        {
+            var r = UnityEngine.Random.value * (totalWeight + entry.weight); // random value
+            if (r >= totalWeight) // probability of this is weight/(totalWeight+weight)
+                selected = entry; // it is the probability of discarding last selected element and selecting current one instead
+            totalWeight += entry.weight; // increase weight sum
+        }
+        Assert.IsNotNull(selected);
+
+        return selected;
+    }
+}
